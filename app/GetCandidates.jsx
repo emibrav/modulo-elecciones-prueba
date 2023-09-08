752e196363a36f5fa55046246696ae36512a1613
Emiliano BRavo <bravoemiliano74@gmail.com>
2023-09-08 18:19:24 -0300

    revalidate

--- app/GetCandidates.jsx
+++ app/GetCandidates.jsx
@@ -5,28 +5,23 @@ import Image from "next/image"
 import "./globals.css"
 
 const fetchResults = () => {
-  return axios
-    .get(
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vQK1MR-HUUIj8rXpby9uGFU7BgbZo2pHJlpZ6-FgFDpu8_kDJDwBbWNxG-U2Au_ZuMcp9kffuvhwZON/pub?output=csv",
-      {
-        responseType: "blob",
-      },
-      {
-        next: {
-          revalidate: 10,
-        },
+  return fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQK1MR-HUUIj8rXpby9uGFU7BgbZo2pHJlpZ6-FgFDpu8_kDJDwBbWNxG-U2Au_ZuMcp9kffuvhwZON/pub?output=csv", { next: { revalidate: 120 } })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok")
       }
-    )
-    .then((res) => {
+      return response.text() // Cambiamos a response.text() para obtener el texto
+    })
+    .then((text) => {
       return new Promise((resolve, reject) => {
-        Papa.parse(res.data, {
+        Papa.parse(text, {
           header: true,
           complete: (results) => {
             console.log(results.data)
-            return resolve(results.data)
+            resolve(results.data)
           },
           error: (error) => {
-            return reject(error.message)
+            reject(error.message)
           },
         })
       })
@@ -46,5 +41,4 @@ const GetCandidates = async () => {
     </div>
   )
 }
-
 export default GetCandidates
