fc300b2f964b261877ed04eac66ef4e558a68a51
Emiliano BRavo <bravoemiliano74@gmail.com>
2023-10-08 21:09:56 -0300

    .env

--- app/GetCandidates.jsx
+++ app/GetCandidates.jsx
@@ -2,7 +2,7 @@ import Papa from "papaparse"
 import Card from "./Card"
 
 const fetchResults = () => {
-  return fetch("https://docs.google.com/spreadsheets/d/1IXw8QNVwQ540csFzPfcnPe84lKMJNJ5z80mJYdfKQ1M/pub?output=csv", { next: { revalidate: 60 } })
+  return fetch(process.env.SHEET_URL, { next: { revalidate: 30 } })
     .then((response) => {
       if (!response.ok) {
         throw new Error("Fetching error")
@@ -15,7 +15,6 @@ const fetchResults = () => {
           header: true,
           complete: (results) => {
             resolve(results.data)
-            // console.log(results.data[0]["Mesas Escrutadas"])
           },
           error: (error) => {
             reject(error.message)
@@ -24,22 +23,11 @@ const fetchResults = () => {
       })
     })
 }
-// const mesasEscrutadas = results[0]["Mesas Escrutadas"]
-const formatNumber = (numberString) => {
-  const number = parseFloat(numberString.replace(",", ".")) // Reemplaza la coma por el punto y convierte a número
-  return number.toLocaleString("es-ES") // Formatea el número con separadores de miles y decimales
-}
 
 const GetCandidates = async () => {
   const results = await fetchResults()
   return (
     <>
-      {/* <div className='px-3'>
-        <p className='text-xs'>
-          Escrutado: {formatNumber(results[0]["Mesas Escrutadas"])}% <br></br>
-          En blanco: {formatNumber(results[0]["Votos en Blanco"])} | Votos totales: {formatNumber(results[0]["Total de Votos"])}
-        </p>
-      </div> */}
       <Card results={results} />
     </>
   )
